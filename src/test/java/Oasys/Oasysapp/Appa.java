286b9ae89ba51cb8aecde0cc235d0474dbff9768
radhakrushna <radhakrushnasatapathy7461@gmail.com>
2024-07-06 22:02:42 +0530

    this is source file...

--- src/test/java/Oasys/Oasysapp/Appa.java
+++ src/test/java/Oasys/Oasysapp/Appa.java
@@ -33,7 +33,7 @@ public class Appa {
 		 }
 		 
 		driver.manage().window().maximize();
-		driver.get("http://bimsdemo.demoapplication.in/");
+		driver.get("https://cams.odisha.gov.in/");
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);	
 		
 	}
@@ -45,126 +45,19 @@ public class Appa {
 //		driver.get("http://bimsdemo.demoapplication.in/");
 //		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		Thread.sleep(2000);
-		driver.findElement(By.xpath("//a[text()='Create an account']")).click();
+		driver.findElement(By.xpath("//*[@id='btnseed2']")).click();
 		
 	}
 	int count=0;
 	@Test(dataProvider="testdata",dataProviderClass=ReadExcelFile.class)
-	public void enterFirstname(String id,String firstname,String lastname,String District,String block,String policestattion,String village,String phoneno,String email) throws InterruptedException, IOException
+	public void enterFirstname(String username,String pass) throws InterruptedException, IOException
 	{
 		
-//		Thread.sleep(3000);
-//		driver.findElement(By.xpath("//input[@name='FirstName']")).clear();
-//		Thread.sleep(3000);
-		System.out.println(firstname);
-		 wait=new WebDriverWait(driver,60);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='FirstName']")));
-		
-		driver.findElement(By.xpath("//input[@name='FirstName']")).sendKeys(firstname);
-		
-		/* Thread.sleep(2000); */
-		 wait=new WebDriverWait(driver,60);
-			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='lName']")));
-		driver.findElement(By.xpath("//input[@id='lName']")).sendKeys(lastname);
-		
-//		Thread.sleep(2000);
-		wait=new WebDriverWait(driver,60);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='eMail']")));
-		driver.findElement(By.xpath("//input[@id='eMail']")).sendKeys(email);
-		
-		
-		System.out.println(phoneno);
-		
-//		Thread.sleep(3000);
-		wait=new WebDriverWait(driver,60);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='mNo']")));
-		driver.findElement(By.xpath("//input[@id='mNo']")).sendKeys(phoneno);
-		
-//		Thread.sleep(2000);
-		wait=new WebDriverWait(driver,60);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='txtAdharNo']")));
-		driver.findElement(By.xpath("//input[@id='txtAdharNo']")).sendKeys("123456789012");
-		
-		Thread.sleep(2000);
-
-
-	System.out.println(District.substring(0,District.length()-2));
-	String d=District.substring(0,District.length()-2);
-		WebElement district=driver.findElement(By.xpath("//*[@id='ddlDistrict']"));
-		wait=new WebDriverWait(driver,60);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='ddlDistrict']")));
-		select=new Select(district);
-		select.selectByValue(d);
-		
-		Thread.sleep(2000);
-		String b=block.substring(0,block.length()-2);
-		WebElement blockname=driver.findElement(By.xpath("//*[@id='ddlBlock']"));
-		wait=new WebDriverWait(driver,60);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='ddlBlock']")));
-		select=new Select(blockname);
-		select.selectByValue(b);
-		
-		Thread.sleep(2000);
-		String p=policestattion.substring(0,policestattion.length()-2);
-		System.out.println(policestattion);
-		
-		WebElement policestation=driver.findElement(By.xpath("//*[@id='ddlPolice']"));
-		wait=new WebDriverWait(driver,60);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='ddlPolice']")));
-		select=new Select(policestation);
-		select.selectByValue(p);
-		
-		Thread.sleep(2000);
-		
-		driver.findElement(By.xpath("//*[@id='txtArea']")).sendKeys(village);
-		
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("//*[@id='txtVillage']")).sendKeys("Test");
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("//*[@id='txtPin']")).sendKeys("123456");
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("//*[@id='pWd']")).sendKeys("Oasys@1234");
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("//*[@id='cPwd']")).sendKeys("Oasys@1234");
-		
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("//*[@id='submitButton']")).click();
-		Thread.sleep(5000);
-		count++;
-		
-		
-		
-		
-		String confirmation_message=driver.findElement(By.xpath("//*[@class='swal2-popup swal2-modal swal2-show']/div[2]")).getText();
-		System.out.println("print  message ="+confirmation_message);
-		if(confirmation_message.equals("Contact number is already in use.")) {
-			
-			driver.findElement(By.xpath("//*[text()='OK']")).click();
-			
-		}
-		else {
-			driver.findElement(By.xpath("//*[text()='OK']")).click();
-		}
-		
-	
-		
-		Thread.sleep(2000);
+		System.out.println("print parameter"+username+" "+pass);
+		++count;
+		Thread.sleep(1000);
+		driver.findElement(By.id("txtFirstName")).sendKeys("rama"+count);
 		driver.navigate().refresh();
-		
-		
-		//create object of readexcelfile
-		readexcel=new ReadExcelFile();
-		readexcel.inserAndEditCell(confirmation_message,count);
-
-		
-//		FileOutputStream fis=new FileOutputStream(System.getProperty("user.dir")+"//Externaldata//Data3.xlsx");
-		
-		
-		
-		
-		
-
-		
 		
 	}
 	
--- src/test/java/Oasys/Oasysapp/ReadExcelFile.java
+++ src/test/java/Oasys/Oasysapp/ReadExcelFile.java
@@ -48,7 +48,7 @@ public class ReadExcelFile {
 //		File file=new File(System.getProperty("user.dir") + "//DataExcel//Data.xlsx");
 
 //		try {
-		fis = new FileInputStream(System.getProperty("user.dir") + "//Externaldata//BimsDataAuto1.xlsx");
+		fis = new FileInputStream(System.getProperty("user.dir") + "//Externaldata//nameandpass.xlsx");
 		System.out.println("this is executed");
 //	s
 
@@ -70,25 +70,30 @@ public class ReadExcelFile {
 		cellSize = Integer.valueOf(cellValue);
 
 		System.out.println("RowSize=" + rowsize);
-		System.out.println("CellSize" + cellSize);
+		System.out.println("CellSize=" + cellSize);
+		
 
 		// initialize the Object[][].
-		object = new String[rowsize + 1][9];
+		object = new String[rowsize + 1][2];
+		
+		
 
+//
 		// store all the excel value to two dimensional array which is object[][].
-		for (int i = 1; i <= rowsize; i++) {
-			for (int j = 1; j < 9; j++) {
-				if (j == 7 || j == 5 || j == 4 || j == 3) {
-					System.out.println(xssfSheet.getRow(i).getCell(j).getNumericCellValue());
-					double value = xssfSheet.getRow(i).getCell(j).getNumericCellValue();
-//					System.out.println("This is for only 7 cell"+value);
-					object[i][j] = String.valueOf(xssfSheet.getRow(i).getCell(j).getNumericCellValue());
-//					System.out.println("This is for only 7 cell");
-				} else {
+		for (int i = 0; i <= rowsize; i++) {
+			for (int j = 0; j < cellSize; j++) {
+//				if (j==) {
+//					System.out.println(xssfSheet.getRow(i).getCell(j).getNumericCellValue());
+//					double value = xssfSheet.getRow(i).getCell(j).getNumericCellValue();
+////					System.out.println("This is for only 7 cell"+value);
+//					object[i][j] = String.valueOf(xssfSheet.getRow(i).getCell(j).getNumericCellValue());
+////					System.out.println("This is for only 7 cell");
+//				} 
+				
 					System.out.println(xssfSheet.getRow(i).getCell(j).getStringCellValue());
 					object[i][j] = xssfSheet.getRow(i).getCell(j).getStringCellValue();
 //					System.out.println("This is for all");
-				}
+				
 
 			}
 
@@ -96,8 +101,8 @@ public class ReadExcelFile {
 //		
 		System.out.println("row number" + object[1].length);
 		// return this
-		for (int i = 1; i <= rowsize; i++) {
-			for (int j = 0; j < 9; j++) {
+		for (int i = 0; i <= rowsize; i++) {
+			for (int j = 0; j < cellSize; j++) {
 				System.out.println("object print" + object[i][j]);
 			}
 		}
@@ -106,120 +111,120 @@ public class ReadExcelFile {
 
 	}
 
-	public void inserAndEditCell(String result,int index) throws IOException {
-		// change-1
-		fis = new FileInputStream(System.getProperty("user.dir") + "//Externaldata//BimsDataAuto1.xlsx");
-		xssfworkbook = new XSSFWorkbook(fis);
-		xssfSheet = xssfworkbook.getSheet("Sheet1");
-		System.out.println("Row num=" + xssfSheet.getLastRowNum());
-		System.out.println("cell value=" + xssfSheet.getRow(index).getLastCellNum());
-//		int lastcellvalue=xssfSheet.getRow(0).getLastCellNum()-1;
-//		System.out.println("last cell value="+lastcellvalue);
-		
-		
-		int count = 0;
-		Iterator<Cell> eachcell = xssfSheet.getRow(0).cellIterator();
-	
-		
-//		while(eachcell.hasNext()) {			
-//			System.out.println("cell number=" + (count++) + "cell type=" + eachcell.next().getCellType()+"value="+eachcell.next().getStringCellValue());
-//		}
-
-		// create cell if the cell not exit
-		System.out.println("last cell value=" + xssfSheet.getRow(index).getCell(xssfSheet.getRow(index).getLastCellNum() - 1).getStringCellValue());
-		System.out.println(xssfSheet.getRow(0).getCell(xssfSheet.getRow(0).getLastCellNum()- 1).getStringCellValue());
-
-		// create the cell
-		if (xssfSheet.getRow(0).getCell(xssfSheet.getRow(0).getLastCellNum() - 1).getStringCellValue()
-				.equals("Aadhar_VoterNo")) {
-			xssfSheet.getRow(0).createCell(xssfSheet.getRow(0).getLastCellNum()).setCellValue("Status");
-			// change-2
-			FileOutputStream fos = new FileOutputStream(
-					System.getProperty("user.dir") + "//Externaldata//BimsDataAuto1.xlsx");
-			xssfworkbook.write(fos);
-			fos.close();
-//		System.out.println("Successfull write in excel....");
-//		System.out.println("get cell value="+xssfSheet.getRow(0).getCell(xssfSheet.getRow(0).getLastCellNum()).getCellType());
-		}
-		
-		int lastcellvalue=xssfSheet.getRow(index).getLastCellNum();
-		System.out.println("last cell value="+lastcellvalue);
-		
-		fis.close();
-		fis = new FileInputStream(System.getProperty("user.dir") + "//Externaldata//BimsDataAuto1.xlsx");
-		xssfworkbook = new XSSFWorkbook(fis);
-		xssfSheet = xssfworkbook.getSheet("Sheet1");
-		System.out.println("Row num=" + xssfSheet.getLastRowNum());
-		System.out.println("cell size=" + xssfSheet.getRow(1).getLastCellNum());
-		System.out.println("cell value="+xssfSheet.getRow(index).getCell(xssfSheet.getRow(index).getLastCellNum()-1).getStringCellValue());
-		
-		if(xssfSheet.getRow(1).getCell(xssfSheet.getRow(1).getLastCellNum()-1).getStringCellValue()=="") {
-			System.out.println("printing hhhhh...");
-		}
-		
-		
-		
-		System.out.println(
-				"get cell Type=" + xssfSheet.getRow(index).getCell(xssfSheet.getRow(index).getLastCellNum() - 1).getCellType());
-		System.out.println("get cell value="
-				+ xssfSheet.getRow(0).getCell(xssfSheet.getRow(0).getLastCellNum() - 1).getStringCellValue());
-
-		// set value the cell
-		if (xssfSheet.getRow(0).getCell(xssfSheet.getRow(0).getLastCellNum() - 1).getStringCellValue()
-				.equals("Status")) {
-			// remove cell
-//		xssfCell1=xssfSheet.getRow(0).getCell(xssfSheet.getRow(0).getLastCellNum()-1);
-//		xssfSheet.getRow(0).removeCell(xssfCell1);
+//	public void inserAndEditCell(String result,int index) throws IOException {
+//		// change-1
+//		fis = new FileInputStream(System.getProperty("user.dir") + "//Externaldata//BimsDataAuto1.xlsx");
+//		xssfworkbook = new XSSFWorkbook(fis);
+//		xssfSheet = xssfworkbook.getSheet("Sheet1");
+//		System.out.println("Row num=" + xssfSheet.getLastRowNum());
+//		System.out.println("cell value=" + xssfSheet.getRow(index).getLastCellNum());
+////		int lastcellvalue=xssfSheet.getRow(0).getLastCellNum()-1;
+////		System.out.println("last cell value="+lastcellvalue);
 //		
-			// here we set the cell value
-			xssfSheet.getRow(0).getCell(xssfSheet.getRow(0).getLastCellNum() - 1).setCellValue("Result output");
-
-			// change-3
-			FileOutputStream fileOut = new FileOutputStream(
-					System.getProperty("user.dir") + "//Externaldata//BimsDataAuto1.xlsx");
-			xssfworkbook.write(fileOut);
-			fileOut.close();
-	
-		}
-		//fis.close();
-		
-		
-		//
+//		
+//		int count = 0;
+//		Iterator<Cell> eachcell = xssfSheet.getRow(0).cellIterator();
+//	
+//		
+////		while(eachcell.hasNext()) {			
+////			System.out.println("cell number=" + (count++) + "cell type=" + eachcell.next().getCellType()+"value="+eachcell.next().getStringCellValue());
+////		}
+//
+//		// create cell if the cell not exit
+//		System.out.println("last cell value=" + xssfSheet.getRow(index).getCell(xssfSheet.getRow(index).getLastCellNum() - 1).getStringCellValue());
+//		System.out.println(xssfSheet.getRow(0).getCell(xssfSheet.getRow(0).getLastCellNum()- 1).getStringCellValue());
+//
+//		// create the cell
+//		if (xssfSheet.getRow(0).getCell(xssfSheet.getRow(0).getLastCellNum() - 1).getStringCellValue()
+//				.equals("Aadhar_VoterNo")) {
+//			xssfSheet.getRow(0).createCell(xssfSheet.getRow(0).getLastCellNum()).setCellValue("Status");
+//			// change-2
+//			FileOutputStream fos = new FileOutputStream(
+//					System.getProperty("user.dir") + "//Externaldata//BimsDataAuto1.xlsx");
+//			xssfworkbook.write(fos);
+//			fos.close();
+////		System.out.println("Successfull write in excel....");
+////		System.out.println("get cell value="+xssfSheet.getRow(0).getCell(xssfSheet.getRow(0).getLastCellNum()).getCellType());
+//		}
+//		
+//		int lastcellvalue=xssfSheet.getRow(index).getLastCellNum();
+//		System.out.println("last cell value="+lastcellvalue);
+//		
+//		fis.close();
 //		fis = new FileInputStream(System.getProperty("user.dir") + "//Externaldata//BimsDataAuto1.xlsx");
 //		xssfworkbook = new XSSFWorkbook(fis);
 //		xssfSheet = xssfworkbook.getSheet("Sheet1");
 //		System.out.println("Row num=" + xssfSheet.getLastRowNum());
 //		System.out.println("cell size=" + xssfSheet.getRow(1).getLastCellNum());
 //		System.out.println("cell value="+xssfSheet.getRow(index).getCell(xssfSheet.getRow(index).getLastCellNum()-1).getStringCellValue());
-		System.out.println("cell type="+xssfSheet.getRow(index).getCell(lastcellvalue-1).getCellType());
-
-		//create cell for status cell
-		if (xssfSheet.getRow(index).getCell(lastcellvalue-1).getCellType()
-				.equals(CellType.BLANK)) {
-			xssfSheet.getRow(index).createCell(lastcellvalue-1).setCellValue(result);
-			FileOutputStream fos = new FileOutputStream(
-					System.getProperty("user.dir") + "//Externaldata//BimsDataAuto1.xlsx");
-			xssfworkbook.write(fos);
-			fos.close();
-		}
-		
-		//xssfSheet.getRow(index).getLastCellNum() - 1
-		
-		
-		else if(!xssfSheet.getRow(index).getCell(lastcellvalue-1).getCellType()
-				.equals(CellType.BLANK)) {
-
-			System.out.println("print index="+index);
-			System.out.println("cell num="+xssfSheet.getRow(index).getLastCellNum());
-			xssfSheet.getRow(index).getCell(lastcellvalue-1).setCellValue("Orange"+result+" "+"apple");
-			FileOutputStream fileOut = new FileOutputStream(
-					System.getProperty("user.dir") + "//Externaldata//BimsDataAuto1.xlsx");
-			xssfworkbook.write(fileOut);
-			fileOut.close();
-			System.out.println("updated......");
-			
-		}
-
-	}
+//		
+//		if(xssfSheet.getRow(1).getCell(xssfSheet.getRow(1).getLastCellNum()-1).getStringCellValue()=="") {
+//			System.out.println("printing hhhhh...");
+//		}
+//		
+//		
+//		
+//		System.out.println(
+//				"get cell Type=" + xssfSheet.getRow(index).getCell(xssfSheet.getRow(index).getLastCellNum() - 1).getCellType());
+//		System.out.println("get cell value="
+//				+ xssfSheet.getRow(0).getCell(xssfSheet.getRow(0).getLastCellNum() - 1).getStringCellValue());
+//
+//		// set value the cell
+//		if (xssfSheet.getRow(0).getCell(xssfSheet.getRow(0).getLastCellNum() - 1).getStringCellValue()
+//				.equals("Status")) {
+//			// remove cell
+////		xssfCell1=xssfSheet.getRow(0).getCell(xssfSheet.getRow(0).getLastCellNum()-1);
+////		xssfSheet.getRow(0).removeCell(xssfCell1);
+////		
+//			// here we set the cell value
+//			xssfSheet.getRow(0).getCell(xssfSheet.getRow(0).getLastCellNum() - 1).setCellValue("Result output");
+//
+//			// change-3
+//			FileOutputStream fileOut = new FileOutputStream(
+//					System.getProperty("user.dir") + "//Externaldata//BimsDataAuto1.xlsx");
+//			xssfworkbook.write(fileOut);
+//			fileOut.close();
+//	
+//		}
+//		//fis.close();
+//		
+//		
+//		//
+////		fis = new FileInputStream(System.getProperty("user.dir") + "//Externaldata//BimsDataAuto1.xlsx");
+////		xssfworkbook = new XSSFWorkbook(fis);
+////		xssfSheet = xssfworkbook.getSheet("Sheet1");
+////		System.out.println("Row num=" + xssfSheet.getLastRowNum());
+////		System.out.println("cell size=" + xssfSheet.getRow(1).getLastCellNum());
+////		System.out.println("cell value="+xssfSheet.getRow(index).getCell(xssfSheet.getRow(index).getLastCellNum()-1).getStringCellValue());
+//		System.out.println("cell type="+xssfSheet.getRow(index).getCell(lastcellvalue-1).getCellType());
+//
+//		//create cell for status cell
+//		if (xssfSheet.getRow(index).getCell(lastcellvalue-1).getCellType()
+//				.equals(CellType.BLANK)) {
+//			xssfSheet.getRow(index).createCell(lastcellvalue-1).setCellValue(result);
+//			FileOutputStream fos = new FileOutputStream(
+//					System.getProperty("user.dir") + "//Externaldata//BimsDataAuto1.xlsx");
+//			xssfworkbook.write(fos);
+//			fos.close();
+//		}
+//		
+//		//xssfSheet.getRow(index).getLastCellNum() - 1
+//		
+//		
+//		else if(!xssfSheet.getRow(index).getCell(lastcellvalue-1).getCellType()
+//				.equals(CellType.BLANK)) {
+//
+//			System.out.println("print index="+index);
+//			System.out.println("cell num="+xssfSheet.getRow(index).getLastCellNum());
+//			xssfSheet.getRow(index).getCell(lastcellvalue-1).setCellValue("Orange"+result+" "+"apple");
+//			FileOutputStream fileOut = new FileOutputStream(
+//					System.getProperty("user.dir") + "//Externaldata//BimsDataAuto1.xlsx");
+//			xssfworkbook.write(fileOut);
+//			fileOut.close();
+//			System.out.println("updated......");
+//			
+//		}
+//
+//	}
 
 }
